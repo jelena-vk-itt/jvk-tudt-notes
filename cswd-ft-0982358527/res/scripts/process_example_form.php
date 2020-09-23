abb234660e0d02b8600b20db0e3ce46f372fb48a
Jelena <jelena.vasic@tudublin.ie>
2020-09-23 15:27:33 +0100

    updates to timetable

--- cswd-ft-0982358527/res/scripts/process_example_form.php
+++ cswd-ft-0982358527/res/scripts/process_example_form.php
@@ -12,21 +12,23 @@
   <body>
 
     <h1>Hi <span class="red"><?php
-			       function test_input($data) {
-			       $data = trim($data);
-			       $data = stripslashes($data);
-			       $data = htmlspecialchars($data);
-			       return $data;
-			       }
-			       
-			       $firstname = $middlename = $surname = "";
-			       
-			       if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			       $firstname = test_input($_POST["firstname"]);
-			       $middlename = test_input($_POST["middlename"]);
-			       $surname = test_input($_POST["surname"]);
-			       }
-			       
-			       echo "". $firstname . " " . $middlename . " " . $surname;?></span>, thanks for entering your name.</h1>
+      function test_input($data) {
+          $data = trim($data);
+          $data = stripslashes($data);
+          $data = htmlspecialchars($data);
+          return $data;
+      }
+      
+      $firstname = $middlename = $surname = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstname = test_input($_POST["firstname"]);
+    $middlename = test_input($_POST["middlename"]);
+    $surname = test_input($_POST["surname"]);
+} else {
+    echo "---" . $_SERVER["REQUEST_METHOD"] . "---";
+}
+
+echo "". $firstname . " " . $middlename . " " . $surname;?></span>, thanks for entering your name.</h1>
   </body>
 </html> 
