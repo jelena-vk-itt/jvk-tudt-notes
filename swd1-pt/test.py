6d40a83c4a5b05516d72804f2d78929343a1e1ab
Jelena Vasic <jelena.vasic@tudublin.ie>
2020-09-29 01:51:20 +0100

    offline notes

---
+++ swd1-pt/test.py
@@ -0,0 +1,6 @@
+x = input("Enter a whole number: ")
+y = input("Enter another whole number: ")
+# x and y are both of type string
+print("The sum of your numbers without conversion is ", x + y)
+# int(x) and int(y) will be integers and so will their sum
+print("The sum of your numbers with conversion is ", int(x) + int(y))
