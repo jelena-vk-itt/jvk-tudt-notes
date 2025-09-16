b2683784d606750edae37609be69db50c0738a5d
Jelena Vasić <jelena.vasic@tudublin.ie>
2025-09-16 12:21:36 +0100

    test

--- ooswd/res/files/lab_solutions/a2e2.py
+++
@@ -1,8 +0,0 @@
-# read the name and surname
-nameAndSurname = input("Please enter your name and surname, separated by a space: ")
-
-# split into name and surname as separate elements of a list
-nameAndSurnameList = nameAndSurname.split()
-
-# print the required information in the required format
-print("{0}, {1}".format(nameAndSurnameList[1].capitalize(), nameAndSurnameList[0][0].capitalize()))
--- ooswd/res/files/lab_solutions/a3e1.py
+++
@@ -1,8 +0,0 @@
-namesList = input("Please input the names of the players: ").split()
-scoresList = list(map(int, input("Please input the players' scores: ").split()))
-print("The highest score is ", max(scoresList))
-print("The lowest score is ", min(scoresList))
-print("The average score is ", sum(scoresList) / len(scoresList))
-print("List of players in order of their scores: ", ' '.join(sorted(namesList, key=lambda name : scoresList[namesList.index(name)], reverse=True)))
-
-
